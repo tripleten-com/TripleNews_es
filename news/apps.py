579dadb4b8208d30c244959050e5f164d865a4cd
e-statsenko <114163693+e-statsenko@users.noreply.github.com>
2024-02-14 15:28:45 +0300

    Update apps.py
    added es

--- news/apps.py
+++ news/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class NewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'news'
-    verbose_name = 'News'
+    name = 'noticia'
+    verbose_name = 'Noticia'
